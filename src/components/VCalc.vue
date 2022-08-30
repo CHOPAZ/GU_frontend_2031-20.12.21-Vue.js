7591cac82cf8e72eaa518951f899b827add1f04a
Pavel Kvasnitsky <paxa627894@yandex.ru>
2022-08-31 00:15:22 +0500

    Hw.vue#1

---
+++ src/components/VCalc.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+    <input type="number" v-model.number="op1">
+    <input type="number" v-model.number="op2">
+    = {{ result }}
+    <div class="keyboard">
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="div">/</button>
+      <button @click="mult">*</button>
+      <button @click="expon">Возведение в степень</button>
+      <button @click="trunc">Целочисленное деление</button>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'VCalc',
+  data: () => ({
+    op1: 0,
+    op2: 0,
+    result: 0
+  }),
+
+  methods: {
+    sum () {
+      this.result = this.op1 + this.op2
+    },
+    sub () {
+      this.result = this.op1 - this.op2
+    },
+    div () {
+      this.result = this.op1 / this.op2
+    },
+    mult () {
+      this.result = this.op1 * this.op2
+    },
+    expon () {
+      this.result = Math.pow(this.op1, this.op2)
+    },
+    trunc () {
+      this.result = Math.trunc(this.op1 / this.op2)
+    }
+
+  }
+}
+</script>
